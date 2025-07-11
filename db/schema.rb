482ab97db8d1d3cda4fc5fdb59653e0b020a90c6
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-11 10:22:56 -0400

    add status attribute to migration.

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_10_150257) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
