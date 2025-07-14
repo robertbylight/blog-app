6be291df293c74bb5ed362e37e0934273b5f10e4
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-14 15:20:43 -0400

    add more test cases for controller

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :request do
   describe 'GET /articles' do
-    it 'returns a successful response' do
+    before do
       get articles_path
+    end
+
+    it 'returns a successful response' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns json' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+
+    it 'returns articles and meta keys in response' do
+      json_response = JSON.parse(response.body)
+
+      expect(json_response).to include('articles', 'meta')
     end
   end
 end
