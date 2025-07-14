fba93458686a3d10001a348df243d180bbfafa0c
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-14 09:27:38 -0400

    add pagination to controller.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,18 @@ class ArticlesController < ApplicationController
   def index
     render json: articles
   end
+
   private
 
   def articles
-    Article.all
+    is_using_pagination = params[:page].present? || params[:per_page].present?
+
+    if is_using_pagination
+      page = params[:page].to_i > 0 ? params[:page].to_i : 1
+      per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 2
+      Article.paginate(page: page, per_page: per_page)
+    else
+      Article.all
+    end
   end
 end
