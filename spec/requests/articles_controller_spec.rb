dc87c4ac2ef2eee932c130ea839b69a8d1e95f9e
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-15 10:19:55 -0400

    add sorting tests cases.

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :request do
   describe 'GET /articles' do
-      let(:article_one) { Article.create(title: "First", body: "body 1", status: "published") }
-      let(:article_two) { Article.create(title: "Second", body: "body 2", status: "draft") }
+      let(:article_one) { Article.create(title: 'First', body: 'body 1', status: 'published') }
+      let(:article_two) { Article.create(title: 'Second', body: 'body 2', status: 'draft') }
 
     before do
       article_one
@@ -31,23 +31,23 @@ RSpec.describe ArticlesController, type: :request do
       expect(json_response['articles'].length).to eq(2)
     end
 
-    it 'returns articles with correct content and status' do
+    it 'returns articles with correct content' do
       json_response = JSON.parse(response.body)
 
       articles = [
         {
           'id' => article_one.id,
-          'title' => "First",
-          'body' => "body 1",
-          'status' => "published",
+          'title' => 'First',
+          'body' => 'body 1',
+          'status' => 'published',
           'created_at' => article_one.created_at.as_json,
           'updated_at' => article_one.updated_at.as_json
         },
         {
           'id' => article_two.id,
-          'title' => "Second",
-          'body' => "body 2",
-          'status' => "draft",
+          'title' => 'Second',
+          'body' => 'body 2',
+          'status' => 'draft',
           'created_at' => article_two.created_at.as_json,
           'updated_at' => article_two.updated_at.as_json
         }
@@ -56,15 +56,15 @@ RSpec.describe ArticlesController, type: :request do
       expect(json_response['articles']).to eq(articles)
     end
 
-    context 'with pagination' do
+    context 'pagination' do
       before do
         Article.destroy_all
 
         10.times do |i|
           Article.create(
-            title: "Article #{i}",
-            body: "Body #{i}",
-            status: "published"
+            title: 'Article #{i}',
+            body: 'Body #{i}',
+            status: 'published'
           )
         end
         get articles_path, params: { page: 3, per_page: 4 }
@@ -82,6 +82,58 @@ RSpec.describe ArticlesController, type: :request do
           'total_pages' => 3,
           'total_articles' => 10
         )
+      end
+    end
+
+    context 'sorting' do
+      context 'sorting by title' do
+        before do
+          Article.destroy_all
+          Article.create(title: 'C', body: 'c body', status: 'published')
+          Article.create(title: 'A', body: 'a body', status: 'published')
+          Article.create(title: 'B', body: 'b body', status: 'draft')
+        end
+
+        it 'sorts articles by title' do
+          get articles_path, params: { sort_by: 'title', sort_order: 'asc' }
+          json_response = JSON.parse(response.body)
+          titles = json_response['articles'].map { |article| article['title'] }
+
+          puts titles
+
+          expect(titles).to eq([ 'A', 'B', 'C' ])
+        end
+
+        it 'sorts articles by title' do
+          get articles_path, params: { sort_by: 'title', sort_order: 'desc' }
+          json_response = JSON.parse(response.body)
+          titles = json_response['articles'].map { |article| article['title'] }
+
+          expect(titles).to eq([ 'C', 'B', 'A' ])
+        end
+      end
+
+      context 'sort by created_at' do
+        before do
+          Article.destroy_all
+          Article.create(title: 'first', body: 'classic', status: 'archived', created_at: 2.days.ago)
+          Article.create(title: 'second', body: 'middle aged', status: 'not relevant anymore', created_at: 1.day.ago)
+          Article.create(title: 'third', body: 'new born', status: 'published')
+        end
+
+        it 'sorts by created_at desc' do
+          get articles_path, params: { sort_by: 'created_at', sort_order: 'desc' }
+          json_response = JSON.parse(response.body)
+          titles = json_response['articles'].map { |article| article['title'] }
+          expect(titles).to eq([ 'third', 'second', 'first' ])
+        end
+
+        it 'sorts by created_at asc' do
+          get articles_path, params: { sort_by: 'created_at', sort_order: 'asc' }
+          json_response = JSON.parse(response.body)
+          titles = json_response['articles'].map { |article| article['title'] }
+          expect(titles).to eq([ 'first', 'second', 'third' ])
+        end
       end
     end
   end
