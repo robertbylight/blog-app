950c325cb14722d3c6a7c64fdd5775dce93132ce
robertbylight <107279507+robertbylight@users.noreply.github.com>
2025-07-18 21:43:12 -0400

    deleted old test since this project is using rspec.

--- test/controllers/articles_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "index route should be successful" do
-    get articles_path
-    assert_response :success
-  end
-end
--- test/models/article_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ArticleTest < ActiveSupport::TestCase
-  test "should save article without errors" do
-    article = Article.new(title: "test title", body: "test body")
-    assert article.save
-  end
-end
